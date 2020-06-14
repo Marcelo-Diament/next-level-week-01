c12d3ce1c3786641cc272fd799fa40f5ffa7145a
Marcelo Diament <marcelodiament@gmail.com>
2020-06-14 16:40:34 -0300

    making point and items relation

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -15,6 +15,7 @@ routes.get('/items', async (request, response) => {
     // Serializes items to make client-side returns more accessible
     const serializedItems = items.map(item => {
         return {
+            id: item.id,
             title: item.title,
             image_url: `http://localhost:3333/uploads/${item.image}`
         }
@@ -22,6 +23,50 @@ routes.get('/items', async (request, response) => {
 
     // Returns items
     return response.json(serializedItems);
+})
+
+// A post rout to create new point
+routes.post('/points', async (request, response) => {
+    // Creating point properties with destructuring syntax (same as const item = request.body.item for each os the properties)
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    // Creatig a transaction that avoids register insert if any of following querires gets an error
+    const trx = await knex.transaction();
+
+    // Defining 'create' props with knex (except by items, that are foreign registers) - using short syntax (once name and value of property is the same)
+    const insertedIds = await knex('points').insert({
+        image: 'fake-image',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    });
+
+    const point_id = insertedIds[0];
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id: point_id
+        }
+    });
+
+    // Inserts point_id to point_items table (fk/pk)
+    await trx('point_items').insert(pointItems);
+
+    return response.json({ success: true});
 })
 
 // Exports routes so they can be accessed by app
