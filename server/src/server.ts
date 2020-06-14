97103f159baa836a9906b9537c679cfb3b709331
Marcelo Diament <marcelodiament@gmail.com>
2020-06-14 18:46:53 -0300

    adding cors to server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import path from 'path';
-
-// Imports routes files
 import routes from './routes';
 
-// Creates express app with its methods
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use(routes);
-
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
