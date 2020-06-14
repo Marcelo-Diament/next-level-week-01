9f2d6da95536f5f4737c09e719765266b0e20ada
Marcelo Diament <marcelodiament@gmail.com>
2020-06-14 19:43:15 -0300

    aula 03 - parte 02C - State

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Header from './Header';
 
 function App() {
+  const [counter, setCounter] = useState(0); // useState always return an Array, like [state value, function that updates its value]
+
+  function handleButtonClick() {
+    setCounter(counter + 1); // setCounter is the second param of useState applied to counter
+  }
+
   return (
     <div>
-      <Header title="Next Level Week #01" />
-      <h2>Conteúdo da Aplicação</h2>
+      <Header title={`Next Level Week #${counter}`} />
+
+      <h2>{counter}</h2>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
     </div>
   );
 }
