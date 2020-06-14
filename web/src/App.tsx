80211ee675f340ecef7797ea3a016953217c9463
Marcelo Diament <marcelodiament@gmail.com>
2020-06-14 20:11:34 -0300

    aula 03 - parte 04 - home

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-
-import Header from './Header';
+import Home from './pages/Home';
 
 function App() {
-  const [counter, setCounter] = useState(0); // useState always return an Array, like [state value, function that updates its value]
-
-  function handleButtonClick() {
-    setCounter(counter + 1); // setCounter is the second param of useState applied to counter
-  }
-
   return (
-    <div>
-      <Header title={`Next Level Week #${counter}`} />
-
-      <h2>{counter}</h2>
-      <button type="button" onClick={handleButtonClick}>Aumentar</button>
-    </div>
+    <Home />
   );
 }
 
