7023fab0403188b2a0f0ed1540eaae436064f3dd
Marcelo Diament <marcelodiament@gmail.com>
2020-06-14 19:16:41 -0300

    aula 03 - parte 01 - cleaning unecessary files

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Esse é o componente <code>src/App.tsx</code>.
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Esse é o componente <code>src/App.tsx</code>.</h1>
   );
 }
 
