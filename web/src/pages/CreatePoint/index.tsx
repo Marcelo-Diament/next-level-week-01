f559ca1d3f9e3263b31da0bd2e5bd97769d7f1fe
Marcelo Diament <marcelodiament@gmail.com>
2020-06-14 21:10:28 -0300

    aula 03 - parte 08 - get items from api

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api';
+
 import './styles.css';
 import logo from '../../assets/logo.svg';
 
+interface Item {
+    id: number;
+    title: string;
+    image_url: string;
+}
+
 const CreatePoint = () => {
+    const [items, setItems] = useState<Item[]>([]);
+    
+    useEffect(() => {
+        api.get('items')
+            .then(response => {
+                setItems(response.data);
+            })
+    }, []);
+
     return (
         <div id="page-create-point">
             <header>
@@ -27,7 +44,6 @@ const CreatePoint = () => {
                         <label htmlFor="name">Nome da Entidade</label>
                         <input type="text" name="name" id="name" />
                     </div>
-
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="email">Email</label>
@@ -38,7 +54,6 @@ const CreatePoint = () => {
                             <input type="text" name="whatsapp" id="whatsapp" />
                         </div>
                     </div>
-
                 </fieldset>
                 <fieldset>
                     <legend>
@@ -71,12 +86,12 @@ const CreatePoint = () => {
                         <span>Selecione os itens aceitados abaixo</span>
                     </legend>
                     <ul className="items-grid">
-                        <li><img src="http://localhost:3333/uploads/baterias.svg" alt="Baterias" /><span>Baterias</span></li>
-                        <li><img src="http://localhost:3333/uploads/eletronicos.svg" alt="Pilhas e Eletrônicos" /><span>Pilhas e Eletrônicos</span></li>
-                        <li><img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas" /><span>Lâmpadas</span></li>
-                        <li><img src="http://localhost:3333/uploads/oleo.svg" alt="Óleo de Cozinha" /><span>Óleo de Cozinha</span></li>
-                        <li><img src="http://localhost:3333/uploads/organicos.svg" alt="Orgânicos" /><span>Orgânicos</span></li>
-                        <li className="selected"><img src="http://localhost:3333/uploads/papeis-papelao.svg" alt="Papéis e Papelão" /><span>Papéis e Papelão</span></li>
+                        {items.map(item => (
+                            <li key={item.id}>
+                                <img src={item.image_url} alt="Baterias" />
+                                <span>{item.title}</span>
+                            </li>
+                        ))}
                     </ul>
                 </fieldset>
             </form>
